6ae7d39c744ec6dc2bba534d5179cbe66eb55f75
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-08-19 19:14:52 +0000

    make another commit

--- server.js
+++ server.js
@@ -5,9 +5,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/",(req,res) => {
   return res.status(200).render("index");
+});
+
+app.get("/contact",(req,res) => {
+  return res.status(200).render("contact");
+});
+
+app.get("/plans",(req,res) => {
+  return res.status(200).render("plans");
+});
+
+app.get("/login",(req,res) => {
+  return res.status(200).render("login");
+});
+
+app.get("/register",(req,res) => {
+  return res.status(200).render("register");
 })
 
 app.listen(PORT, () => {
